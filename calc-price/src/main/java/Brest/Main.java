8c1e11dbfa231c0765525c85fafcd59bf6b772df
AliakseiVaryvada <alv96@bk.ru>
2020-01-31 12:58:44 -0500

    fix: add logic

--- calc-price/src/main/java/Brest/Main.java
+++ calc-price/src/main/java/Brest/Main.java
@@ -1,7 +1,54 @@
 package Brest;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+     Double[] enterValues = new Double[4];
+
+     Scanner scanner = new Scanner(System.in);
+     String inputValue;
+     int i = 0;
+     do{
+         if(i == 0){
+             System.out.println("Please, enter distance or q for exit: ");
+         } else if(i == 1){
+             System.out.println("Please, enter price per km or q for exit: ");
+         } else if(i == 2){
+             System.out.println("Please, enter weight km or q for exit: ");
+         } else if(i == 3){
+             System.out.println("Please, enter price per kg or q for exit: ");
+         }
+
+         inputValue = scanner.next();
+         if(!isExitValue(inputValue)){
+             if(isDoubleValue(inputValue)){
+                 enterValues[i] = Double.parseDouble(inputValue);
+                 i++;
+             }
+         }
+         if(i == 4){
+             Double calcResult = enterValues[0] * enterValues[1] + enterValues[2] * enterValues[3];
+             System.out.println("Price: $"+calcResult);
+             i = 0;
+         }
+     } while (!isExitValue(inputValue));
+        System.out.println("Finish");
+    }
+
+    private static boolean isExitValue(String value){
+        return  value.equalsIgnoreCase("Q");
+    }
+
+    private static boolean isDoubleValue(String value){
+        boolean checkResult = true;
+        try {
+            double enteredValue = Double.parseDouble(value);
+            checkResult = enteredValue >=0;
+        } catch(NumberFormatException ex) {
+            checkResult = false;
+        }
+        return checkResult;
     }
 }
