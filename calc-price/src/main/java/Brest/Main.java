0fe33e51f7b480d795f944b5f2643a2e681532a0
Aliaksei <alv96@bk.ru>
2020-02-07 22:48:34 +0300

    reformat code: create method for invite to input values, fileReader method.

--- calc-price/src/main/java/Brest/Main.java
+++ calc-price/src/main/java/Brest/Main.java
@@ -1,30 +1,25 @@
 package Brest;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
      Double[] enterValues = new Double[4];
-
      Scanner scanner = new Scanner(System.in);
      String inputValue;
+
      int i = 0;
      do{
-         if(i == 0){
-             System.out.println(" enter distance or q for exit: ");
-         } else if(i == 1){
-             System.out.println("Please, enter price per km or q for exit: ");
-         } else if(i == 2){
-             System.out.println("Please, enter weight km or q for exit: ");
-         } else if(i == 3){
-             System.out.println("Please, enter price per kg or q for exit: ");
-         }
-
+         invitationToEnterValue(i);
          inputValue = scanner.next();
+
          if(!isExitValue(inputValue)){
              if(isDoubleValue(inputValue)){
-                 enterValues[i] = Double.parseDouble(inputValue);
+
+                 enterValues[i] = new BigDecimal(coefficientСalculation(i, inputValue));
                  i++;
              }
          }
@@ -36,6 +31,68 @@ public class Main {
      } while (!isExitValue(inputValue));
         System.out.println("Finish");
     }
+
+    private static String getCoefficient(int inputCounter) throws FileNotFoundException {
+
+        FileReader coefficientFileReader = new FileReader("coefficients.txt");
+        Scanner scan = new Scanner(coefficientFileReader);
+
+        int i = 0;
+
+        while (scan.hasNextLine()) {
+            if(i == inputCounter) {
+                return scan.nextLine();
+            } else {
+                i++;
+            }
+        }
+        return "wrong string";
+    }
+
+
+    private static void coefficientСalculation(int inputCounter, int inputValue){
+
+        switch (inputCounter) {
+            case 0:
+
+                String resultStr = str.substring(str.indexOf('.') + 1, str.indexOf(':'));
+                break;
+            case 1:
+                System.out.println("Please, enter price or Q for exit: ");
+                break;
+            case 2:
+                System.out.println("Please enter weight or Q for exit: ");
+                break;
+            case 3:
+                System.out.println("Please, enter price per kg or Q for exit: ");
+                break;
+        }
+    }
+
+
+
+    private static void invitationToEnterValue(int inputCounter) {
+
+        switch (inputCounter) {
+            case 0:
+                System.out.println("Please enter distance or Q for exit: ");
+                break;
+            case 1:
+                System.out.println("Please, enter price or Q for exit: ");
+                break;
+            case 2:
+                System.out.println("Please enter weight or Q for exit: ");
+                break;
+            case 3:
+                System.out.println("Please, enter price per kg or Q for exit: ");
+                break;
+        }
+    }
+
+
+
+
+
 
     private static boolean isExitValue(String value){
         return  value.equalsIgnoreCase("Q");
